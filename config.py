b434af80a2bc50274d86c27e500812a6faaecf0c
nguyendang092 <trungnguyencv092@gmail.com>
2024-07-25 09:38:59 +0700

    Initial commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'Dangnguyen0902@')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
