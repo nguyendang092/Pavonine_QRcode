b988d7402a05bd361ea7fca720943d64fa43b961
nguyendang092 <trungnguyencv092@gmail.com>
2024-07-25 11:38:26 +0700

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from .main import main as main_blueprint
 
-
-app = Flask(__name__)
-app.register_blueprint( url_prefix='/main')
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(main_blueprint, url_prefix='/main')
+    return app
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from . import main
 
 main = Blueprint('main', __name__)
 
