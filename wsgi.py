1a1f30217b064b978e35e3b0569ef78984b22d2e
nguyendang092 <trungnguyencv092@gmail.com>
2024-07-25 11:32:58 +0700

    update

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,6 @@
 from app import create_app
-from dotenv import load_dotenv
-
-
 
 app = create_app()
-load_dotenv()
+
 if __name__ == "__main__":
-    app.run(debug=True)  # This is for local development only
+    app.run()
