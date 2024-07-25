6dfd97894fe46e1d4e54d965897f29fe4d72ca17
nguyendang092 <trungnguyencv092@gmail.com>
2024-07-25 15:22:34 +0700

    update

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,3 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('JavaScript is working!');
-});
+const slides = document.querySelector('.slides');
+const images = document.querySelectorAll('.slides img');
+let index = 0;
+
+function nextSlide() {
+    index = (index + 1) % images.length;
+    slides.style.transform = `translateX(${-index * 100}vw)`;
+}
+
+setInterval(nextSlide, 15000);
