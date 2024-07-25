8d8d6a4c638ec125d3561dc7ad1cb074bdd3f82d
nguyendang092 <trungnguyencv092@gmail.com>
2024-07-25 11:12:03 +0700

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from config import Config
+from .routes import bp
 
-db = SQLAlchemy()
-migrate = Migrate()
-
-def create_app(config_class=Config):
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config_class)
-
-    db.init_app(app)
-    migrate.init_app(app, db)
-
-    from app import routes, models
-    app.register_blueprint(routes.bp)
-
+    app.register_blueprint(bp)
     return app
+
