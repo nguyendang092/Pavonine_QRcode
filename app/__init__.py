64cec305029df35afa4d14532b6cfafcc9eaa37f
nguyendang092 <trungnguyencv092@gmail.com>
2024-07-25 11:35:25 +0700

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .main import main as main_blueprint
+
 
 app = Flask(__name__)
-app.register_blueprint(main_blueprint, url_prefix='/main')
+app.register_blueprint( url_prefix='/main')
