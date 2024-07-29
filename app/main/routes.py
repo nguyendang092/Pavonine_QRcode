8f5282dd08e075e1e04aaebabd33b1398ec97cf7
nguyendang092 <trungnguyencv092@gmail.com>
2024-07-29 16:05:32 +0700

    update

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, session, send_file,request,redirect, url_for, make_response,Blueprint
+import os
+from flask import render_template, session, request, redirect, url_for, Blueprint
 from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
 import pytz
@@ -7,8 +8,9 @@ import base64
 from io import BytesIO
 from . import main
 
-
-
+UPLOAD_FOLDER = 'path/to/storage'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 @main.route('/')
 def index():
@@ -18,7 +20,6 @@ def index():
 @main.route('/about')
 def about():
     return render_template('about.html')
-
 
 def generate_qr(url, timestamp):
     qr_content = f"{url}?data={timestamp}"
@@ -43,50 +44,48 @@ def generate_qr(url, timestamp):
     draw.text(text_position, text, font=font, fill='black')
     return img
 
-@main.route('generate_qr_download')
+@main.route('/generate_qr_download')
 def generate_qr_download():
     timestamp = datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
-    formatted_timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    formatted_timestamp = timestamp.strftime('%Y-%m-%d_%H-%M-%S')  # Replacing ':' with '_'
     url = "https://myprojectflask-f4e65bcb2a22.herokuapp.com/main/scan_qr"
-    qr_name = f'Pavonine_QrCode_{formatted_timestamp}'
+    qr_name = f'Pavonine_QrCode_{formatted_timestamp}.png'
+    qr_path = os.path.join(UPLOAD_FOLDER, qr_name)
     image = generate_qr(url, formatted_timestamp)
+    image.save(qr_path, format="PNG")
 
-    response = make_response(redirect(url_for('main.qr_info')))
-    response.set_cookie('creation_qr', formatted_timestamp)
-    response.set_cookie('qr_name', qr_name)
-
-    buffer = BytesIO()
-    image.save(buffer, format="PNG")
-    buffer.seek(0)
-    session['qr_image'] = buffer.getvalue()
+    session['qr_creation_time'] = formatted_timestamp
+    session['qr_image_path'] = qr_path
     return redirect(url_for('main.qr_info'))
 
-@main.route('/main/qr_info')
+@main.route('/qr_info')
 def qr_info():
-    qr_image = session.get('qr_image')
-    creation_time = session.get('creation_qr')
-    qr_name = session.get('qr_name')
-    print("Creation Time:", creation_time)
+    qr_image_path = session.get('qr_image_path')
+    creation_time = session.get('qr_creation_time')
+    qr_name = os.path.basename(qr_image_path) if qr_image_path else None
 
-    if not qr_image or not creation_time:
+    if not qr_image_path or not creation_time:
         return "QR code not found", 404
-    qr_image_base64 = base64.b64encode(qr_image).decode('utf-8')
+
+    with open(qr_image_path, "rb") as img_file:
+        qr_image_base64 = base64.b64encode(img_file.read()).decode('utf-8')
+
     return render_template('qr_info.html', qr_image=qr_image_base64, creation_time=creation_time, qr_name=qr_name)
 
 @main.route('/scan_qr')
 def scan_qr():
-    creation_time_str = session.get('creation_qr')
+    creation_time_str = session.get('qr_creation_time')
     scan_time = datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
     data = request.args.get('data')
     if data:
-        creartion_qr = datetime.strptime(creation_time_str,'%Y-%m-%d %H:%M:%S')
-        time_diff = scan_time - creartion_qr
-        time_diff_hour = time_diff.total_seconds()/3600
+        creation_qr = datetime.strptime(creation_time_str, '%Y-%m-%d_%H-%M-%S')
+        time_diff = scan_time - creation_qr
+        time_diff_hour = time_diff.total_seconds() / 3600
         if time_diff_hour > 12:
             message = "Mã QR đã đủ 12 giờ. Vui lòng chuyển công đoạn tiếp theo"
             return render_template('scan_qr.html', message=message)
         else:
-            message = "Mã QR chưa đủ 12 giờ. Vui lòng đợi thêm {scan_time} - {creartion_qr}"
+            message = f"Mã QR chưa đủ 12 giờ. Vui lòng đợi thêm {scan_time} - {creation_qr}"
     else:
         message = "No data in URL."
-        return render_template('scan_qr.html', message=message)
+    return render_template('scan_qr.html', message=message)
