a3bfadf387b5420e7de67c9c51e3490107f5fe06
nguyendang092 <trungnguyencv092@gmail.com>
2024-08-03 16:37:54 +0700

    update

--- app/main/routes.py
+++ app/main/routes.py
@@ -48,10 +48,10 @@ def generate_qr_download():
     timestamp = datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
     formatted_timestamp = timestamp.strftime('%Y_%m_%d_%H_%M_%S')
     name_qrcode = timestamp.strftime('%Y_%m_%d %H_%M_%S')
-    url = f"https://myprojectflask-f4e65bcb2a22.herokuapp.com/main/scan_qr/{formatted_timestamp}"
+    url = f"https://myprojectflask-f4e65bcb2a22.herokuapp.com/main/scan_qr"
     qr_name = f'Pavonine_QRcode_{formatted_timestamp}.png'
     qr_path = os.path.join(UPLOAD_FOLDER, qr_name)
-    image = generate_qr(url)
+    image = generate_qr(url,formatted_timestamp)
     image.save(qr_path, format="PNG")
 
     session['qr_creation_time'] = formatted_timestamp
