87d314c20c1f2acd0a317fbab34725f196e7a7ef
nguyendang092 <trungnguyencv092@gmail.com>
2025-04-10 13:39:35 +0700

    update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\QLSX\OneDrive\Desktop\my_flask_app\venv
+set VIRTUAL_ENV=C:\Users\QLSX\OneDrive\Desktop\QRcode_Pavo\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
